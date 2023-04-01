66835ec9e8ac2889aab41cf6edc311a8b6f9f911
tripathismt <72719255+tripathismt@users.noreply.github.com>
2023-04-01 13:37:51 +0530

    first commit

---
+++ todolist/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+const Header = () => {
+
+    return (
+        <header>
+           <div className="hcontainer">
+               <h1>TodoList</h1>
+            </div> 
+        </header>
+    )
+}
+
+export default Header;
